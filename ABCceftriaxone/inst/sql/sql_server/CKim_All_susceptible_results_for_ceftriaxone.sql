f5d5efaeeb49c773674655c75439b1506196c29b
Chungsoo Kim <ted9219@gmail.com>
2021-01-12 17:14:12 +0900

    change the minCovariateRatio, fix person_id to measurement_id

--- ABCceftriaxone/inst/sql/sql_server/CKim_All_susceptible_results_for_ceftriaxone.sql
+++ ABCceftriaxone/inst/sql/sql_server/CKim_All_susceptible_results_for_ceftriaxone.sql
@@ -35,7 +35,7 @@ with primary_events (event_id, person_id, start_date, end_date, op_start_date, o
         JOIN #Codesets codesets on ((m.measurement_concept_id = codesets.concept_id and codesets.codeset_id = 0))
         WHERE measurement_id in (select distinct fact_id_2 from @cdm_database_schema.fact_relationship where domain_concept_id_1 in (3026008))
         and value_source_value = 'S' 
-        and person_id not in (select distinct person_id from @cdm_database_schema.measurement 
+        and measurement_id not in (select distinct measurement_id from @cdm_database_schema.measurement 
           where measurement_concept_id in (3034442, 3022747) 
           and measurement_id in (select distinct fact_id_2 from CDMPv532.dbo.fact_relationship where domain_concept_id_1 in (3026008))
           and value_source_value in ('I', 'R')
